2f21d4f3c20c10f775aac67fbf0b65d63adda8b9
oscar-davids <oscar_davids@outlook.com>
2021-01-19 20:17:56 +0800

    modify packet validity check

--- lpmsapp/lpmsapp.c
+++ lpmsapp/lpmsapp.c
@@ -110,6 +110,131 @@ int isindices(int* indicies, int count, int pkindex)
 	return ret;
 }
 
+/*
+success return 0
+fail	return != 0
+*/
+#define PACKET_188LEN	(188)
+#define PACKET_204LEN	(204)
+#define NULL_PID		(0x1FFF)
+#define TS_HEAD_TAG		(0x47)
+
+#define OFFSET_VALID			(0x80000000)
+#define DISPLAY_VALID			(0x40000000)
+#define GET_VALID				(0x20000000)
+#define DELET_VALID				(0x10000000)
+#define SAVE_VALID				(0x08000000)
+#define PAT_VALID				(0x00000001)
+#define PMT_VALID				(0x00000002)
+#define CAT_VALID				(0x00000004)
+#define NIT_ACTUAL_VALID		(0x00000008)
+#define NIT_OTHER_VALID			(0x00000010)
+#define BAT_VALID				(0x00000020)
+#define SDT_ACTUAL_VALID		(0x00000040)
+#define SDT_OTHER_VALID			(0x00000080)
+#define EIT_ACTUAL_VALID		(0x00000100)
+#define EIT_ACTUAL_SCH_VALID	(0x00000200)
+#define TOT_VALID				(0x00000400)
+#define TDT_VALID				(0x00000800)
+
+
+#define PAT_PID					(0x00)
+#define CAT_PID					(0x01)
+#define NIT_PID					(0x10)
+#define SDT_BAT_PID				(0x11)
+#define EIT_PID					(0x12)
+#define TDT_TOT_PID				(0x14)
+
+int checkvalidation(const char* fname, int* indicies, int* pkpos, int* pklength, int count, char* profile)
+{
+	int bcheck = -1;
+	int ret = 0;
+
+	FILE *fin;
+	uint8_t pkt[188];
+	uint8_t* payload;
+	uint8_t payload_len = 0;
+	uint8_t pes[1024 * 4];
+
+	fin = fopen(fname, "rb");
+	if (!fin) {
+		return -1;
+	}
+
+	for (int i = 0; i < count; i++)
+	{
+		int pkcount = pklength[i] / PACKET_188LEN;
+
+		fseek(fin, pkpos[i], SEEK_SET);
+
+		int pknum = 0;
+		unsigned short packet_pid = NULL_PID;
+		int vcount = 0;
+		int pes_len = -1;
+		
+		while (fread(pkt, 188, 1, fin) == 1 && pknum < pkcount)
+		{
+			if (pkt[0] != 0x47) {
+				ret = -1;
+				break;
+			}
+			//check pid and psi
+			packet_pid = ((pkt[1] & 0x1F) << 8) | (pkt[2] & 0xFF);
+			payload = pkt + 4;
+			payload_len = PACKET_188LEN - 4;
+			//if there is adaptive field
+			if ((pkt[3] >> 5) & 1) {
+				payload += payload[0] + 1;
+			}
+			/*
+			if (packet_pid == PAT_PID || packet_pid == CAT_PID || packet_pid == NIT_PID || packet_pid == SDT_BAT_PID ||
+				packet_pid == EIT_PID || packet_pid == TDT_TOT_PID || packet_pid == 0x1000) continue;
+			*/
+			//pes check
+			if ((pkt[1] >> 6) & 1) {
+				/* Validate the start code */
+				if (pes_len > 0) {
+
+					if (pes[0] != 0x00 || pes[1] != 0x00 || pes[2] != 0x01)
+					{
+						ret = -1;
+						break;
+					}
+
+					if (pes[3] >= 0xE0 && pes[3] <= 0xEF) {
+						vcount++;
+					}
+					if (pes[3] >= 0xC0 && pes[3] <= 0xDF) {
+						ret = -1;
+						break;
+					}
+				}
+				pes_len = 0;
+			}
+			if (pes_len >= 0) {
+
+				if (pes_len + payload_len >= 1024 * 4)
+				{
+					ret = -1;
+					break;
+				}
+
+				memcpy(&pes[pes_len], payload, payload_len);
+				pes_len += payload_len;
+			}
+
+			pknum++;
+		}
+		if (vcount != 1 || ret == -1)
+		{
+			break;
+		}
+	}
+	
+	fclose(fin);
+
+	return ret;
+}
 
 /*
 success return 0
@@ -305,6 +430,7 @@ int parseparam(char* strlists, int* pout)
 }
 //app input.mp4 output.ts P720p25fps16x9 sw
 //app input.mp4 indicies(8,13,...) positions(147956,172020,...) lengths(1504,1316,...) count_of_indicies profilename(P720p30fps16x9)
+//d:/out.ts  10,15 147956,172020 1504,1316 2 P720p60fps16x9
 int main(int argc, char **argv)
 {		
 #ifdef PACKET_VALIDITY
@@ -323,7 +449,8 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	ret  = checkValidity(argv[1], indicies, positions, lengths, count, argv[6]);
+	//ret  = checkValidity(argv[1], indicies, positions, lengths, count, argv[6]);
+	ret = checkvalidation(argv[1], indicies, positions, lengths, count, argv[6]);
 	if (ret == 0)
 	{
 		printf("\nredition validity is OK!\n");
@@ -360,7 +487,7 @@ int main(int argc, char **argv)
 
 	input_params inparam = { argv[1], h, AV_HWDEVICE_TYPE_NONE, NULL };
 	output_params* outparam = (output_params*)malloc(sizeof(output_params) * nout);	
-	AVRational fps = { profile[proid].Framerate, profile[proid].FramerateDen };
+	AVRational fps = { profiles[proid].Framerate, profiles[proid].FramerateDen };
 	
 	char encoder[64] = { 0, };
 	char scale_filter[64] = { 0, };
@@ -381,10 +508,10 @@ int main(int argc, char **argv)
 		strcpy(scale_filter, "scale");
 	}
 	sprintf(vfilters, "%s='w=if(gte(iw,ih),%d,-2):h=if(lt(iw,ih),%d,-2)',fps=%d/%d", 
-		scale_filter, profile[proid].ResolutionW, profile[proid].ResolutionH, profile[proid].Framerate, profile[proid].FramerateDen);
+		scale_filter, profiles[proid].ResolutionW, profiles[proid].ResolutionH, profiles[proid].Framerate, profiles[proid].FramerateDen);
 	
 
-	output_params param = { argv[2], vfilters, profile[proid].ResolutionW, profile[proid].ResolutionH, profile[proid].Bitrate, 0, fps, muxOpts, audioOpts, vidOpts };
+	output_params param = { argv[2], vfilters, profiles[proid].ResolutionW, profiles[proid].ResolutionH, profiles[proid].Bitrate, 0, fps, muxOpts, audioOpts, vidOpts };
 	outparam[0] = param;
 
 	output_results *encresults = (output_results*)malloc(sizeof(output_results) * nout);
